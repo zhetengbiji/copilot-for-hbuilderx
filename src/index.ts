1ab72d7a48b6d1daa38109a919505c1a6fe860e9
qiang <guoshengqiang@live.com>
2023-11-04 21:44:10 +0800

    feat: use registerCompletionItemProvider

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { fork } from 'node:child_process'
-import { JSONRPCClient, JSONRPCServer } from 'json-rpc-2.0'
+// import { JSONRPCClient, JSONRPCServer } from 'json-rpc-2.0'
+import * as rpc from 'vscode-jsonrpc/node'
 import path = require('node:path')
 import * as vscode from 'vscode'
 // @ts-ignore
@@ -10,6 +11,8 @@ try {
   console.warn('hbuilderx not found')
 }
 
+const TIMEOUT = 1e3 * 60
+
 let statusBarItem: vscode.StatusBarItem
 
 const child = fork(path.join(__dirname, '../dist/agent.js'), [
@@ -18,17 +21,55 @@ const child = fork(path.join(__dirname, '../dist/agent.js'), [
 ], {
   stdio: 'pipe'
 })
+const logger = {
+  log(message: string) {
+    console.log(message)
+  },
+  error(message: string) {
+    console.error(message)
+  },
+  warn(message: string) {
+    console.warn(message)
+  },
+  info(message: string) {
+    console.info(message)
+  }
+}
+let connection = rpc.createMessageConnection(new rpc.StreamMessageReader(child.stdout!), new rpc.StreamMessageWriter(child.stdin!), logger)
+connection.listen()
+// connection.inspect()
+// const client = new JSONRPCClient((jsonRPCRequest) => {
+//   console.log(jsonRPCRequest)
+//   const json = Buffer.from(JSON.stringify(jsonRPCRequest), 'utf8')
+//   const length = json.length
+//   const header = `Content-Length: ${length}\r\n\r\n`
+//   child.stdin!.write(header)
+//   child.stdin!.write(json)
+// })
 
-const client = new JSONRPCClient((jsonRPCRequest) => {
-  console.log(jsonRPCRequest)
-  const json = Buffer.from(JSON.stringify(jsonRPCRequest), 'utf8')
-  const length = json.length
-  const header = `Content-Length: ${length}\r\n\r\n`
-  child.stdin!.write(header)
-  child.stdin!.write(json)
-})
+const client = {
+  timeout(timeout: number) {
+    return this
+  },
+  request(method: string, params: any) {
+    console.log('request: ', method, params)
+    return connection.sendRequest<any>(method, params)
+  },
+  rejectAllPendingRequests(message: string) {
 
-const server = new JSONRPCServer()
+  },
+  notify(method: string, params: any) {
+    console.log('notify: ', method, params)
+    connection.sendNotification(method, params)
+  }
+}
+
+// const server = new JSONRPCServer()
+const server = {
+  addMethod(method: string, callback: (...params: any[]) => void) {
+    connection.onRequest(method, callback)
+  }
+}
 server.addMethod('LogMessage', (params) => {
   console.log('LogMessage: ', params)
 })
@@ -39,48 +80,47 @@ server.addMethod('statusNotification', (params) => {
   console.log('statusNotification: ', params)
 })
 
-let all = ''
+// let all = ''
 child.stdout!.on('data', (data) => {
   console.log('stdout: ', data.toString())
-  const content = data.toString()
-  all += content
-  let next = true
-  while (next) {
-    const res = all.match(/Content-Length: (\d+)\r\n\r\n/)
-    if (res) {
-      const index = all.indexOf('\r\n\r\n')
-      const header = all.substring(0, index)
-      const other = Buffer.from(all.substring(index + 4), 'utf8')
-      const length = parseInt(res[1])
-      if (other.length >= length) {
-        all = other.toString('utf8')
-        const body = other.subarray(0, length).toString('utf8')
-        all = other.subarray(length).toString('utf8')
-        const obj = JSON.parse(body)
-        if (obj.id) {
-          client.receive(obj)
-        } else if (obj.method) {
-          server.receive(obj)
-        }
-      } else {
-        next = false
-      }
-    } else {
-      next = false
-    }
-  }
+  // const content = data.toString()
+  // all += content
+  // let next = true
+  // while (next) {
+  //   const res = all.match(/Content-Length: (\d+)\r\n\r\n/)
+  //   if (res) {
+  //     const index = all.indexOf('\r\n\r\n')
+  //     const header = all.substring(0, index)
+  //     const other = Buffer.from(all.substring(index + 4), 'utf8')
+  //     const length = parseInt(res[1])
+  //     if (other.length >= length) {
+  //       all = other.toString('utf8')
+  //       const body = other.subarray(0, length).toString('utf8')
+  //       all = other.subarray(length).toString('utf8')
+  //       const obj = JSON.parse(body)
+  //       if (obj.id) {
+  //         client.receive(obj)
+  //       } else if (obj.method) {
+  //         server.receive(obj)
+  //       }
+  //     } else {
+  //       next = false
+  //     }
+  //   } else {
+  //     next = false
+  //   }
+  // }
 })
 
-child.stdout!.on('end', () => {
-  console.log('end')
-})
+// child.stdout!.on('end', () => {
+//   console.log('end')
+// })
 
-child.stderr!.on('data', (err) => {
-  console.log('err: ', err.toString())
-})
+// child.stderr!.on('data', (err) => {
+//   console.log('err: ', err.toString())
+// })
 
 const workspaces: Record<string, {}> = {}
-let isSignedIn = false
 
 function positionToNumber(position: vscode.Position, source: string): number {
   const lines = source.split('\n')
@@ -92,17 +132,21 @@ function positionToNumber(position: vscode.Position, source: string): number {
   return index
 }
 
-async function get() {
-  const editor = vscode.window.activeTextEditor!
-  let workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath
-  if (hbx) {
-    const editor = await hbx.window.getActiveTextEditor()
-    workspaceFolder = editor.document.workspaceFolder.uri.path
+async function initWorkspace() {
+  let workspaceFolder = '/'
+  try {
+    workspaceFolder = vscode.workspace.workspaceFolders![0].uri.fsPath
+    if (hbx) {
+      const editor = await hbx.window.getActiveTextEditor()
+      workspaceFolder = editor.document.workspaceFolder.uri.path
+    }
+  } catch {
+
   }
   console.log('workspaceFolder:', workspaceFolder)
   let item = workspaces[workspaceFolder]
   if (!item) {
-    await client.request('initialize', {
+    await client.timeout(TIMEOUT).request('initialize', {
       rootPath: workspaceFolder,
       rootUri: workspaceFolder,
       capabilities: {},
@@ -112,7 +156,7 @@ async function get() {
         name: 'Copilot for HBuilderX'
       }
     })
-    await client.request('setEditorInfo', {
+    await client.timeout(TIMEOUT).request('setEditorInfo', {
       editorInfo: {
         name: 'HBuilderX',
         version: ''
@@ -125,143 +169,259 @@ async function get() {
     item = {}
     workspaces[workspaceFolder] = item
   }
-  if (!isSignedIn) {
-    const res = await client.request('checkStatus', {
+  return workspaceFolder
+}
+
+async function signout() {
+  client.rejectAllPendingRequests('cancel')
+  await client.timeout(TIMEOUT).request('signOut', {})
+  updateStatus(STATUS.disable)
+  vscode.window.showInformationMessage('已退出')
+}
+
+enum STATUS {
+  loading,
+  warning,
+  disable,
+  enable,
+}
+let status: STATUS = STATUS.disable
+let loading: boolean = false
+function updateStatus(statusOrLoading: STATUS | boolean) {
+  if (typeof statusOrLoading === 'boolean') {
+    loading = statusOrLoading
+    statusOrLoading = statusOrLoading ? STATUS.loading : status
+  } else {
+    status = statusOrLoading
+    if (loading) {
+      statusOrLoading = STATUS.loading
+    }
+  }
+  switch (statusOrLoading) {
+    case STATUS.loading:
+      statusBarItem.text = '$(copilot-loading)'
+      statusBarItem.tooltip = 'Copilot 加载中...'
+      break
+    case STATUS.warning:
+      statusBarItem.text = '$(copilot-warning)'
+      statusBarItem.tooltip = 'Copilot 加载出错'
+      break
+    case STATUS.disable:
+      statusBarItem.text = '$(copilot-disable)'
+      statusBarItem.tooltip = 'Copilot 未启用'
+      break
+    case STATUS.enable:
+      statusBarItem.text = '$(copilot-enable)'
+      statusBarItem.tooltip = 'Copilot 已启用'
+      break
+  }
+}
+
+async function checkStatus() {
+  updateStatus(true)
+  await initWorkspace()
+  if (status === STATUS.disable) {
+    updateStatus(true)
+    const res = await client.timeout(TIMEOUT).request('checkStatus', {
       // options: { localChecksOnly: true }
     })
     // {"status":"NotSignedIn"}
     // {"status":"OK","user":"zhetengbiji"}
-    if (res.status === 'NotSignedIn') {
-      try {
-        const res = await client.request('signInInitiate', {
+    if (res.status === 'OK') {
+      updateStatus(STATUS.enable)
+    }
+  }
+  updateStatus(false)
+}
+
+async function signin() {
+  if (status === STATUS.disable) {
+    updateStatus(true)
+    client.rejectAllPendingRequests('cancel')
+    try {
+      const res = await client.timeout(TIMEOUT).request('checkStatus', {
+        // options: { localChecksOnly: true }
+      })
+      // {"status":"NotSignedIn"}
+      // {"status":"OK","user":"zhetengbiji"}
+      if (res.status === 'NotSignedIn') {
+        const res = await client.timeout(TIMEOUT).request('signInInitiate', {
 
         })
         // {"status":"PromptUserDeviceFlow","userCode":"XXXX-XXXX","verificationUri":"https://github.com/login/device","expiresIn":899,"interval":5}
         vscode.window.showInformationMessage(`请在浏览器中打开 ${res.verificationUri} 并输入 ${res.userCode} 进行登录`)
-        await client.request('signInConfirm', {
+        await client.timeout(res.expiresIn * 1e3).request('signInConfirm', {
           userCode: res.userCode
         })
-        isSignedIn = true
-      } catch (error) {
-
+        updateStatus(STATUS.enable)
+      } else {
+        updateStatus(STATUS.enable)
       }
-    } else {
-      isSignedIn = true
-    }
-  }
-  // await client.request('getVersion', {})
-  const document = editor.document
-  const uri = document.uri.toString()
-  const fileName = document.fileName
-  const text = document.getText()
-  const languageId = document.languageId
-  // client.notify('textDocument/didOpen', {
-  //   textDocument: {
-  //     text,
-  //     languageId,
-  //     uri,
-  //     version: 0
-  //   }
-  // })
-  // await client.request('setEditorInfo', {
-  //   editorInfo: {
-  //     name: 'HBuilderX',
-  //     version: ''
-  //   },
-  //   editorPluginInfo: {
-  //     name: 'Copilot for HBuilderX',
-  //     version: ''
-  //   }
-  // })
-  // client.notify('textDocument/didChange', {
-  //   contentChanges: [{
-  //     text,
-  //   }],
-  //   textDocument: {
-  //     uri,
-  //     version: 0
-  //   }
-  // })
-  const position = editor.selection.start
-  const res = await client.request('getCompletionsCycling', {
-    doc: {
-      source: text,
-      position: {
-        line: position.line,
-        character: position.character
-      },
-      indentSize: 4,
-      insertSpaces: true,
-      tabSize: 4,
-      version: 0,
-      languageId,
-      uri: fileName,
-      path: fileName,
-      relativePath: path.relative(workspaceFolder, fileName)
-    }
-  })
-  console.log('res: ', res)
-  // client.notify('textDocument/didSave', {
-  //   textDocument: {
-  //     uri
-  //   }
-  // })
-  const completions = res.completions
-  if (completions.length) {
-    const completion = completions[0]
-    // await client.request('notifyAccepted', {
-    //   uuid: completion.uuid
-    // })
-    console.log('completion: ', completion)
-    const position = completion.position
-    const range = completion.range
-    const start = range.start
-    const end = range.end
-    const completionText = completion.text.trimEnd()
-    if (hbx) {
-      const editor = await hbx.window.getActiveTextEditor()
-      const range = {
-        start: positionToNumber(start, text),
-        end: positionToNumber(end, text)
+    } catch (error) {
+      console.error(error)
+      const message = error instanceof Error ? error.message : String(error)
+      if (message !== 'cancel') {
+        vscode.window.showInformationMessage(message)
       }
-      await editor.edit((editBuilder: any) => {
-        editBuilder.replace(range, completionText)
-      })
-    } else {
-      // TODO HBuilderX 兼容有问题
-      await editor.edit((editBuilder) => {
-        editBuilder.replace(new vscode.Range(new vscode.Position(start.line, start.character), new vscode.Position(end.line, end.character)), completionText)
-      })
     }
+    updateStatus(false)
   }
 }
 
-async function signout() {
-  await client.request('signOut', {})
-  isSignedIn = false
-  vscode.window.showInformationMessage('已退出登录')
-}
-
-async function checkStatus() {
-
+async function statusClick() {
+  const res = await vscode.window.showInformationMessage(`是否${status === STATUS.enable ? '退出' : '登录'} Copilot？`, '是', '否')
+  if (res === '是') {
+    status === STATUS.enable ? signout() : signin()
+  }
 }
 
 function activate({ subscriptions }: vscode.ExtensionContext) {
-  subscriptions.push(vscode.commands.registerCommand('copilot.get', () => {
-    get()
-  }))
-  subscriptions.push(vscode.commands.registerCommand('copilot.signout', () => {
-    signout()
-  }))
   const statusCommandId = 'copilot.status'
   subscriptions.push(vscode.commands.registerCommand('copilot.status', () => {
-    checkStatus()
+    statusClick()
   }))
   statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
   statusBarItem.command = statusCommandId
-  statusBarItem.text = 'Copilot'
+  updateStatus(status)
   subscriptions.push(statusBarItem)
   statusBarItem.show()
+  checkStatus()
+  subscriptions.push(vscode.workspace.onDidOpenTextDocument(async function (document) {
+    if (status === STATUS.enable) {
+      await initWorkspace()
+      const uri = document.uri.toString()
+      const text = document.getText()
+      const languageId = document.languageId
+      client.notify('textDocument/didOpen', {
+        textDocument: {
+          text,
+          languageId,
+          uri,
+          version: 0
+        }
+      })
+    }
+  }))
+  subscriptions.push(vscode.workspace.onDidChangeTextDocument(async function ({ document }) {
+    if (status === STATUS.enable) {
+      await initWorkspace()
+      const uri = document.uri.toString()
+      const text = document.getText()
+
+      client.notify('textDocument/didChange', {
+        contentChanges: [{
+          text,
+        }],
+        textDocument: {
+          uri,
+          version: 0
+        }
+      })
+    }
+  }))
+  subscriptions.push(vscode.workspace.onDidSaveTextDocument(async function (document) {
+    if (status === STATUS.enable) {
+      await initWorkspace()
+      const uri = document.uri.toString()
+      client.notify('textDocument/didSave', {
+        textDocument: {
+          uri
+        }
+      })
+    }
+  }))
+  subscriptions.push(vscode.workspace.onDidCloseTextDocument(async function (document) {
+    if (status === STATUS.enable) {
+      await initWorkspace()
+      const uri = document.uri.toString()
+      client.notify('textDocument/didClose', {
+        textDocument: {
+          uri
+        }
+      })
+    }
+  }))
+  const selector = [
+    'vue',
+    'javascript'
+  ]
+  subscriptions.push(vscode.languages.registerCompletionItemProvider(selector, {
+    async provideCompletionItems(document, position, token, context) {
+      const array: vscode.CompletionItem[] = []
+      if (status === STATUS.disable) {
+        signin()
+        return array
+      }
+      updateStatus(true)
+      try {
+        const editor = vscode.window.activeTextEditor!
+        const workspaceFolder = await initWorkspace()
+        // await client.request('getVersion', {})
+        const uri = document.uri.toString()
+        const fileName = document.fileName
+        const text = document.getText()
+        const languageId = document.languageId
+        const res = await client.timeout(TIMEOUT).request('getCompletionsCycling', {
+          doc: {
+            source: text,
+            position: {
+              line: position.line,
+              character: position.character
+            },
+            indentSize: 4,
+            insertSpaces: true,
+            tabSize: 4,
+            version: 0,
+            languageId,
+            uri: fileName,
+            path: fileName,
+            relativePath: path.relative(workspaceFolder, fileName)
+          }
+        })
+        console.log('res: ', res)
+        const completions = res.completions
+        for (let index = 0; index < completions.length; index++) {
+          const completion = completions[index]
+          // await client.request('notifyAccepted', {
+          //   uuid: completion.uuid
+          // })
+          console.log('completion: ', completion)
+          const position = completion.position
+          const range = completion.range
+          const start = range.start
+          const end = range.end
+          const completionText = completion.text.trimEnd()
+          let codeRange: any
+          if (hbx && false) {
+            // const editor = await hbx.window.getActiveTextEditor()
+            codeRange = {
+              start: positionToNumber(start, text),
+              end: positionToNumber(end, text)
+            }
+            // await editor.edit((editBuilder: any) => {
+            //   editBuilder.replace(range, completionText)
+            // })
+          } else {
+            codeRange = new vscode.Range(new vscode.Position(start.line, start.character), new vscode.Position(end.line, end.character))
+            // TODO HBuilderX 兼容有问题
+            // await editor.edit((editBuilder) => {
+            //   editBuilder.replace(range, completionText)
+            // })
+          }
+          array.push({
+            label: completionText,
+            insertText: completionText,
+            range: codeRange
+          })
+        }
+      } catch (error) {
+
+      }
+      updateStatus(false)
+      console.log('array: ', array)
+      return array
+    }
+  }))
 }
 
 function deactivate() { }
