04b7145beeac524ddeb555f0ae80c19d5edbf811
qiang <guoshengqiang@live.com>
2024-01-16 22:25:58 +0800

    chore: v0.7.1

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const EDITOR_NAME = 'HBuilderX'
 export const EDITOR_PLUGIN_NAME = 'GitHub Copilot for HBuilderX'
 export const COPILOT_NAME = 'GitHub Copilot'
-export const VERSION = '0.7.0'
+export const VERSION = '0.7.1'
