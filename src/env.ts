f9f951b672d941fa73e4b184fe9def927c764f73
qiang <guoshengqiang@live.com>
2024-01-21 17:49:43 +0800

    chore: v0.7.3

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import * as path from 'node:path'
 export const EDITOR_NAME = 'HBuilderX'
 export const EDITOR_PLUGIN_NAME = 'GitHub Copilot for HBuilderX'
 export const COPILOT_NAME = 'GitHub Copilot'
-export const VERSION = '0.7.2'
+export const VERSION = '0.7.3'
 export const HTML_PATH = path.join(__dirname, '../www/index.html')
 export const HOMEPAGE =
   'https://github.com/zhetengbiji/copilot-for-hbuilderx#readme'
