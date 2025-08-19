d1f1b9647b05fdc8ec560b782e291e8e4dc4cc93
qiang <guoshengqiang@live.com>
2025-08-20 02:57:42 +0800

    chore: v0.7.9

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import * as path from 'node:path'
 export const EDITOR_NAME = 'HBuilderX'
 export const EDITOR_PLUGIN_NAME = 'GitHub Copilot for HBuilderX'
 export const COPILOT_NAME = 'GitHub Copilot'
-export const VERSION = '0.7.8'
+export const VERSION = '0.7.9'
 export const HTML_PATH = path.join(__dirname, '../www/index.html')
 export const CSS_PATH = path.join(__dirname, '../www/github.css')
 export const CSS_DARK_PATH = path.join(__dirname, '../www/github-dark.css')
